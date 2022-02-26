077e6f3505b5f38cae2ef41b35b8e007eaaf941b
Sujiv Raj <59267376+sujiv1204@users.noreply.github.com>
2022-02-27 00:22:47 +0530

    add example

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,21 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
+                <div class="p-6 mt-2 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                    <div class="shrink-0">
+                        <img
+                            class="h-12 w-12"
+                            src={logo}
+                            alt={"ChitChat Logo"}
+                        />
+                    </div>
+                    <div>
+                        <div class="text-xl font-medium text-black">
+                            ChitChat
+                        </div>
+                        <p class="text-slate-500">You have a new message!</p>
+                    </div>
+                </div>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
