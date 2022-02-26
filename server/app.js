f995ed753d2fa3598b4657107ed4806a2378afe6
Sujiv Raj <59267376+sujiv1204@users.noreply.github.com>
2022-02-27 00:04:39 +0530

    Create Boilerplate

---
+++ server/app.js
@@ -0,0 +1,57 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
+
+//session storage
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+//session storage middleware
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+        resave: false,
+        saveUninitialized: true,
+        cookie: { path: "/", secure: false },
+    })
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
